062a3cb4a7a3cd8a133732adc97f6eeb65d02509
Pieter Steyaert <subtiv@gmail.com>
2022-09-16 19:48:16 +0200

    init

---
+++ Moody3.0/Moody3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Moody3_0App.swift
+//  Moody3.0
+//
+//  Created by Pieter Steyaert on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Moody3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
